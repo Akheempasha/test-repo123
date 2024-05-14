6bff145ba99f228666e76031e759bdfcc6515c8f
Akheempasha <akheempashamd@gmail.com>
2024-05-14 23:05:11 +0530

    my files java

---
+++ src/programs/Demo.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class Demo {
+
+	public static void main(String[] args) {
+     
+		int num = 10;
+		long num1 = 0B1010;
+		
+		System.out.println(num == num1);
+	}
+
+}
