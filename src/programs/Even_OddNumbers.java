c38398821784faec8cfa3151f46e77c4876ca040
Akheempasha <akheempashamd@gmail.com>
2024-05-15 07:01:12 +0530

    EvenNumber

---
+++ src/programs/Even_OddNumbers.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class Even_OddNumbers {
+
+	public static void main(String[] args) {
+
+for(int i=1; i<=10; i++) {
+	if(i%2==0) {
+
+	System.out.println("Print Even Numbers.."+i);
+}
+}
+	}
+
+}
